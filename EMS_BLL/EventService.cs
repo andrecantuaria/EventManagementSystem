8b3903de3e1db8f43eb0a2b985d3005063300ece
Andre Cantuaria <146020004+andrecantuaria@users.noreply.github.com>
2024-02-29 23:26:22 -0600

    Add files via upload

---
+++ EMS_BLL/EventService.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using EMS_ENTITIES;
+using EMS_DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS_BLL
+{
+    public class EventService
+    {
+        public List<Event> GetEvents()
+        {
+            List<Event> events = new List<Event>();
+            EventRepository er = new EventRepository();
+            events = er.GetEvents();
+            return events;
+        }
+
+        public bool AddEventServices(Event events)
+        {
+            EventRepository er = new EventRepository();
+            return er.AddEvent(events);
+        }
+
+        public bool UpdateEventService (Event events)
+        {
+            EventRepository er = new EventRepository();
+            return er.UpdateEvent(events);
+        }
+
+        public bool DeleteEventService (int eventID)
+        {
+            EventRepository er = new EventRepository();
+            return er.DeleteEvent(eventID);
+
+        }
+
+
+
+
+
+    }
+}
---
+++ EMS_BLL/RegistrationService.cs
@@ -0,0 +1,40 @@
+﻿using EMS_DAL;
+using EMS_ENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS_BLL
+{
+    public class RegistrationService
+    {
+        public List<Registration> GetRegistrations(int EventID)
+        {
+            List<Registration> registrations = new List<Registration>();
+            RegistrationRepository rr = new RegistrationRepository();
+            registrations = rr.GetRegistrations(EventID);
+            return registrations;
+        }
+
+        public bool AddRegistrationServices(Registration registrations)
+        {
+            RegistrationRepository rr = new RegistrationRepository();
+            return rr.AddRegistration(registrations);
+        }
+
+        public bool UpdateRegistrationService(Registration registrations)
+        {
+            RegistrationRepository rr = new RegistrationRepository();
+            return rr.UpdateRegistration(registrations);
+        }
+
+        public bool DeleteRegistrationService(int RegistrationID, int EventID)
+        {
+            RegistrationRepository rr = new RegistrationRepository();
+            return rr.DeleteRegistration(RegistrationID, EventID);
+        }
+
+    }
+}
