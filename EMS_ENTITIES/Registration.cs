bd60d9985b63a4de781129afa333b7bc97308519
Andre Cantuaria <146020004+andrecantuaria@users.noreply.github.com>
2024-02-29 23:27:30 -0600

    Add files via upload

---
+++ EMS_ENTITIES/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS_ENTITIES
+{
+    public class Registration
+    {
+        public int EventID { get; set; }
+        public int RegistrationID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string PaymentStatus { get; set; }
+    }
+}
